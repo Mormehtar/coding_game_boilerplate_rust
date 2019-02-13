ebe9d7fb731a2a6534d37754402be6eed4849729
darkrider <darkrider@yandex-team.ru>
2019-02-13 23:02:56 +0300

    initial

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate coding_game_boilerplate_rust;
+use coding_game::local_io::ReadBuffer;
+use std::io;
+use coding_game::utils;
+use coding_game::strategy::base_strategy::BaseStrategy;
+use coding_game::strategy::Strategy;
+
+fn main() {
+    let stdin = io::stdin();
+    let mut buffer = Box::new(stdin.lock());
+    let mut reader = ReadBuffer::new(buffer);
+    let stdout = io::stdout();
+
+    utils::main::<BaseStrategy, io::StdoutLock>(reader, &mut stdout.lock());
+}
